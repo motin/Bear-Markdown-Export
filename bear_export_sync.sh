a26f9e4a46fe24212be217a78090883a92e4bcd8
rovest <roarvest@gmail.com>
2018-11-15 21:10:59 +0530

    Updated with better examples with two runs to different out paths

--- bear_export_sync.sh
+++ bear_export_sync.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-/usr/local/bin/python3 ~/Scripts/python/Bear-Markdown-Export/bear_export_sync.py -d > ~/BearTemp/LaunchD_log.txt
+/usr/local/bin/python3 ~/GitHub/Bear-Markdown-Export/bear_export_sync.py -o "Dropbox" -a -s -x private,secret,.shortcuts
+/usr/local/bin/python3 ~/GitHub/Bear-Markdown-Export/bear_export_sync.py -o "OneDrive" -a -s -t writings,travel,.drafts
+
+# Or with run status to log-file:
+# /usr/local/bin/python3 ~/Scripts/python/Bear-Markdown-Export/bear_export_sync.py -o "Dropbox" -a -s > ~/BearTemp/LaunchD_log1.txt
